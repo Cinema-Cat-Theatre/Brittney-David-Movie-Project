149ff0604c3c385866edb3cf9c5eb8c6a0b4b4a7
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-02 16:47:01 -0500

    added more html and ignored key file

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,163 +1,38 @@
-[
-    {
-        "id": 6
-    },
-    {
-        "id": 7
-    },
-    {
-        "id": 8
-    },
-    {
-        "id": 9
-    },
-    {
-        "id": 10
-    },
-    {
-        "id": 11
-    },
-    {
-        "id": 257
-    },
-    {
-        "id": 258
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 260
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 261
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 262
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 263
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 264
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 265
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 272
-    },
-    {
-        "title": 1,
-        "director": "Codey",
-        "rating": 5,
-        "genre": "This is a really good place for coding and eating",
-        "id": 273
-    },
-    {
-        "title": "Pineapple Express",
-        "rating": "5",
-        "id": 274
-    },
-    {
-        "title": "",
-        "rating": "1",
-        "id": 275
-    },
-    {
-        "title": "",
-        "rating": "1",
-        "id": 276
-    },
-    {
-        "title": "",
-        "rating": "1",
-        "id": 277
-    },
-    {
-        "title": "",
-        "rating": "1",
-        "id": 278
-    },
-    {
-        "title": "",
-        "rating": "1",
-        "id": 279
-    },
-    {
-        "title": "hello",
-        "genre": "world",
-        "rating": "4",
-        "director": "bob",
-        "plot": "bill",
-        "id": 280
-    },
-    {
-        "title": "Step Brothers",
-        "rating": "5",
-        "id": 281
-    },
-    {
-        "title": "Resident Evil",
-        "genre": "",
-        "rating": "",
-        "director": "",
-        "plot": "",
-        "id": 282
-    },
-    {
-        "title": "Step Brothers",
-        "rating": "5",
-        "id": 283
-    },
-    {
-        "title": "Step Brothers",
-        "rating": "5",
-        "id": 284
-    },
-    [
-        {
-            "movie": "a",
-            "rating": "b"
-        }
-    ],
-    [
-        {
-            "title": "a",
-            "rating": "5"
-        }
-    ],
-    {
-        "title": "x",
-        "id": 287
-    },
-    {
-        "plot": "An Army Captain is sent to assassinate a Colonel in his own army.",
-        "id": 288
-    }
-]
+"use strict";
+
+(function () {
+//  https://northern-magenta-cashew.glitch.me/movies
+
+/* TODO
+    clean up database
+    On page load:
+Display a "loading..." message
+Make an AJAX request to get a listing of all the movies
+When the initial AJAX request comes back, remove the "loading..." message and replace it with HTML generated from the json response your code receives
+---> an html element who's display attribute changes from block to none upon api success.  Then show all movies as cards in a carousel? https://getbootstrap.com/docs/5.2/components/carousel/ having global var that = currently displayed movie and have buttons under carousel that do what is requested below
+Allow users to add new movies
+Create a form for adding a new movie that has fields for the movie's title and rating
+When the form is submitted, the page should not reload / refresh, instead, your javascript should make a POST request to /movies with the information the user put into the form
+---> Simple post
+Allow users to edit existing movies
+Give users the option to edit an existing movie
+A form should be pre-populated with the selected movie's details
+Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
+---> if movie is showing in carousel, then "edit" button will populate and submit button puts data
+Delete movies
+Each movie should have a "delete" button
+When this button is clicked, your javascript should send a DELETE request - have a "are you sure?" modal: https://getbootstrap.com/docs/5.2/components/modal/#how-it-works
+Bonuses
+Add a disabled attribute to buttons while their corresponding ajax request is still pending.
+no problem
+Show a loading animation instead of just text that says "loading...".  css animation - https://getbootstrap.com/docs/5.2/components/spinners/#about
+Use modals for the creating and editing movie forms.https://getbootstrap.com/docs/5.2/components/modal/#how-it-works
+Add a genre property to every movie.  look up genres in omdb api
+Allow users to sort the movies by rating, title, or genre (if you have it).  kinda like coffee project?
+Allow users to search through the movies by rating, title, or genre (if you have it). kinda like coffee project?
+Use a free movie API like OMDB to include extra info or render movie posters.  Posters are $1 / month, not free
+Helpful Hints
+The id property of every movie should not be edited by hand. The purpose of this property is to uniquely identify that particular movie. That is, if we want to delete or modify an existing movie, we can specify what movie we want to change by referencing it's id. When a new movie is created (i.e. when you send a POST request to /movies with a title and a rating), the server will respond with the movie object that was created, including a generated id.     --->  have an array that is updated after each action
+Take a look at the other branches in this repository, as they have configuration/setup for common scenarios, such as including bootstrap in your application.   ---> what branches?
+  */
+}());
